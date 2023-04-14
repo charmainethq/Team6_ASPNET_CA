4cefa1de28a870b588262ab8d7cbbe36228de031
Charmaine <charmaine.thq@gmail.com>
2023-04-14 16:02:56 +0800

    Added Data Models and CustomerData ADO logic

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,15 +8,11 @@ namespace Team6.Models
     public class Product
     {
 
-        [Key]
         public string ProductId { get; set; }
         public string Name { get; set; }
 
         public string Description { get; set; }
         public float Price { get; set; }
-
-        //One product can be linked to many OrderItems
-        public virtual ICollection<OrderItem> OrderItems { get; set; }
 
     }
 }
