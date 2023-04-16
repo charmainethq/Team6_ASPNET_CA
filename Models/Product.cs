7969a3d3835dcdf207a9b635fd45d47ed3704beb
Charmaine <charmaine.thq@gmail.com>
2023-04-16 20:55:02 +0800

    added images and ProductImage attribute to Product Model

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,7 +11,7 @@ namespace Team6.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public float Price { get; set; }
-
+        public string ProductImage { get; set; }
 
     }
 }
