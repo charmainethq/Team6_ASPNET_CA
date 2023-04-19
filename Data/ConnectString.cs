1677290f6a0c70b0f72ae2b3571beed82554dcb8
Su Latt Sandi <e1114083@u.nus.edu>
2023-04-19 20:04:57 +0800

    qty button works but wip on total price and cart total

--- Data/ConnectString.cs
+++ Data/ConnectString.cs
@@ -1,10 +1,6 @@
 ﻿namespace Team6.Data
 {
     public class ConnectString { 
-
-
-        public static string connectionString = @"Server=(local);Database=ShoppingDB;Integrated Security=true;encrypt=false";
-
-
+        public static string connectionString = @"Server=DESKTOP-6MB3O7O\SQLEXPRESS;Database=ShoppingDB;Integrated Security=true;encrypt=false";
     }
 }
